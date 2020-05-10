c1d006661a1a96ac952631558abd8ea04cb6748f
davindersharma <davindersharma@users.noreply.github.com>
2020-05-10 03:53:42 -0400

    Updates

---
+++ precache-manifest.6a018c38bd3e257f8ea468965c868d03.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd410d9ebce571604687619e1a0e4231",
+    "url": "/online-grocery-store/index.html"
+  },
+  {
+    "revision": "cb728b6ec432ef8272cf",
+    "url": "/online-grocery-store/static/css/2.049b10f6.chunk.css"
+  },
+  {
+    "revision": "13e4a8efb5e701cfd787",
+    "url": "/online-grocery-store/static/css/main.883218d2.chunk.css"
+  },
+  {
+    "revision": "cb728b6ec432ef8272cf",
+    "url": "/online-grocery-store/static/js/2.e5c9019d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/online-grocery-store/static/js/2.e5c9019d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e4a8efb5e701cfd787",
+    "url": "/online-grocery-store/static/js/main.f41f430c.chunk.js"
+  },
+  {
+    "revision": "6e54bd8a7cda18ebf6b4",
+    "url": "/online-grocery-store/static/js/runtime-main.cf5d4ed4.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/online-grocery-store/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "dd49588ab0c9e47f40713d9156486ced",
+    "url": "/online-grocery-store/static/media/product-1.dd49588a.jpg"
+  },
+  {
+    "revision": "01e5e955fa685f0c07856928849e98e3",
+    "url": "/online-grocery-store/static/media/product-2.01e5e955.jpg"
+  },
+  {
+    "revision": "5a7b2f94a839340d89fa444c66ae2da8",
+    "url": "/online-grocery-store/static/media/product-3.5a7b2f94.jpg"
+  },
+  {
+    "revision": "05e112a075e178c4bce458b004bac43a",
+    "url": "/online-grocery-store/static/media/product-4.05e112a0.jpg"
+  },
+  {
+    "revision": "3741cae03e6ab21c719f9d9540671453",
+    "url": "/online-grocery-store/static/media/product-5.3741cae0.jpg"
+  },
+  {
+    "revision": "811b173a942222307a0889db1368efc4",
+    "url": "/online-grocery-store/static/media/product-6.811b173a.jpg"
+  },
+  {
+    "revision": "07476020ad8cb92a77c2b4fe65baa055",
+    "url": "/online-grocery-store/static/media/product-7.07476020.jpg"
+  },
+  {
+    "revision": "b5d30c9fe17eccda2ef608b75703713b",
+    "url": "/online-grocery-store/static/media/product-8.b5d30c9f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/online-grocery-store/precache-manifest.fb39feaf02589f0864197d8184991c2e.js"
+  "/online-grocery-store/precache-manifest.6a018c38bd3e257f8ea468965c868d03.js"
 );
 
 self.addEventListener('message', (event) => {
